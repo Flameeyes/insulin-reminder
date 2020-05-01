8f93e6985b1172b8c03c8018a392889110312fab
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-05-01 21:20:34 +0100

    Basic build and setup.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+; SPDX-FileCopyrightText: © 2020 The insulin-reminder Authors
+; SPDX-License-Identifier: MIT
+((nil . ((fill-column . 88)
+	 (whitespace-line-column . 88)))
+ (markdown-mode . ((mode . flyspell)))
+ (python-mode . ((mode . flyspell-prog)
+		 (mode . whitespace))))
