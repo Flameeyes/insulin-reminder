21f10aef19679db0f4f2d75c225756660ae0aaad
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-05-10 12:29:08 +0100

    Make the Feather connection to WiFi more stable.
    When the WiFi is disconnected, for whatever reason, the next request will
    fail with a cryptic error. To avoid leaving the wrong notification on, if
    that particular error is received, try re-connecting to WiFi.
    
    (It would be easier to reboot, but that doesn't seem safe when connected to
    a computer.)
    
    Also make it a bit easier to see when the WiFi is connecting, by changing
    the colour of the light slightly.

--- feather/code.py
+++ feather/code.py
@@ -37,38 +37,50 @@ esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 if esp.status != adafruit_esp32spi.WL_IDLE_STATUS:
     print("Something went wrong.")
 
-print("Firmware vers.", esp.firmware_version)
+print("Firmware vers. %s" % esp.firmware_version)
 print("MAC addr: %02x:%02x:%02x:%02x:%02x:%02x" % tuple(esp.MAC_address))
 
-
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets.WIFI_SECRETS)
-wifi.connect()
-
-print("Done!")
-
-requests.set_socket(adafruit_esp32spi_socket, esp)
-
 while True:
-    try:
-        response = requests.get(secrets.URL)
-        response_parsed = response.json()
+    # While connecting set the light purpleish.
+    insulin_light.color = (156, 0, 255)
 
-        if "led" in response_parsed:
-            led_color = int(response_parsed["led"])
+    wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets.WIFI_SECRETS)
+    wifi.connect()
 
-        if "refresh" in response_parsed:
-            refresh_time = int(response_parsed["refresh"])
-        else:
-            refresh_time = 60
+    print("Connected to %s" % secrets.WIFI_SECRETS["ssid"])
 
-        print(
-            "Last LED status: #%06x. Refresh time: %d seconds"
-            % (led_color, refresh_time)
-        )
+    requests.set_socket(adafruit_esp32spi_socket, esp)
 
-        insulin_light.color = led_color
-        response.close()
-        time.sleep(refresh_time)
-    except Exception as e:
-        print(str(e))
-        pass
+    while True:
+        try:
+            response = requests.get(secrets.URL)
+            response_parsed = response.json()
+
+            if "led" in response_parsed:
+                led_color = int(response_parsed["led"])
+
+            if "refresh" in response_parsed:
+                refresh_time = int(response_parsed["refresh"])
+            else:
+                refresh_time = 60
+
+            print(
+                "Last LED status: #%06x. Refresh time: %d seconds"
+                % (led_color, refresh_time)
+            )
+
+            insulin_light.color = led_color
+            response.close()
+            time.sleep(refresh_time)
+        except Exception as e:
+            # This is an annoying way to detect the dropped WiFi. If for whatever reason
+            # the WiFi dropped (e.g. rebooted Access Point), the `requests.get()` call
+            # will fail, as it can't read data.
+            #
+            # This leads to a RuntimeError() with the following string being returned over
+            # and over.
+            if str(e) == "Expected 01 but got 00":
+                print("WiFi disconnected, retrying.")
+                break
+            print(str(e))
+            pass
